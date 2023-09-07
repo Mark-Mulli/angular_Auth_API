8271949741a2ec135d2e0c34505de4b1952ad92f
Mark Mulli <markmulli001@gmail.com>
2023-09-07 13:37:50 +0300

    updates on API

--- angular_auth_API/Program.cs
+++ angular_auth_API/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using angular_auth_API.context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<appDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnStr"));
+});
 
 var app = builder.Build();
 
---
+++ angular_auth_API/context/appDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using angular_auth_API.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace angular_auth_API.context
+{
+	public class appDbContext: DbContext
+	{
+        public appDbContext(DbContextOptions<appDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
+
