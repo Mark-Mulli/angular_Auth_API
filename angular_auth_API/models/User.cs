5c71daceddfe4ec50fcd81234013fd15c365016e
Mark Mulli <markmulli001@gmail.com>
2023-09-07 12:57:05 +0300

    updates on API

---
+++ angular_auth_API/models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace angular_auth_API.models
+{
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Token { get; set; }
+
+        public string Role { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
+
